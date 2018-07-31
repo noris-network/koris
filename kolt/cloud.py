0ad95eeb7cccce70e6ac156c1299a5a8f2711519
Oz Tiram <oz.tiram@noris.de>
2018-07-31 17:21:28 +0200

    Remove duplicate code

--- kolt/cloud.py
+++ kolt/cloud.py
@@ -217,40 +217,6 @@ class MasterInit(BaseInit):
 
         return config
 
-    def __str__(self):
-
-        if self.cluster_info:
-            sub_message = MIMEText(self.get_files_config(),
-                                   _subtype='text/cloud-config')
-            sub_message.add_header('Content-Disposition', 'attachment')
-            self.combined_message.attach(sub_message)
-
-        k8s_bootstrap = "bootstrap-k8s-%s-%s-%s.sh" % (self.role,
-                                                       self.os_type,
-                                                       self.os_version)
-
-        # process bootstrap script and generic cloud-init file
-        for item in ['generic', k8s_bootstrap]:
-            fh = open(resource_filename(Requirement('kolt'),
-                                        os.path.join('kolt',
-                                                     'cloud-inits',
-                                                     item)))
-            # we currently blindly assume the first line is a mimetype
-            # or a shebang
-            main_type, _subtype = fh.readline().strip().split("/", 1)
-
-            if '#!' in main_type:
-                _subtype = 'x-shellscript'
-            #    fh.seek(0)
-
-            sub_message = MIMEText(fh.read(), _subtype=_subtype)
-            sub_message.add_header('Content-Disposition',
-                                   'attachment', filename="%s" % item)
-            self.combined_message.attach(sub_message)
-            fh.close()
-
-        return self.combined_message.as_string()
-
 
 class NodeInit(BaseInit):
 
@@ -368,7 +334,7 @@ class NodeInit(BaseInit):
         """) + self._get_kubelet_config() \
              + self._get_certificate_info() \
              + self._get_svc_account_info() \
+             + self._get_kubeconfig() \
              + self._get_calico_config() \
-             + self._get_kubeconfig()
 
         return config
