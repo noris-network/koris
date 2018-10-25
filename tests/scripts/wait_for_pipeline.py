683275286268d928bd626d4cce5d647c1310f365
Oz N Tiram <oz.tiram@gmail.com>
2018-10-25 21:47:08 +0200

    List volumes and job
    Listing running pipeline is good, but it can be that some job screwed
    up, and the clean up phase didn't run. If this is the case, we don't
    want to run and wait for the resources to be free.

--- tests/scripts/wait_for_pipeline.py
+++ tests/scripts/wait_for_pipeline.py
@@ -3,8 +3,12 @@ import os
 import time
 
 from urllib.parse import urlparse
-
 import gitlab
+
+
+from kolt.cloud.openstack import get_clients
+
+_, _, cinder = get_clients()
 
 URL = urlparse(os.getenv("CI_PROJECT_URL"))
 gl = gitlab.Gitlab(URL.scheme + "://" + URL.hostname,
@@ -14,5 +18,12 @@ gl = gitlab.Gitlab(URL.scheme + "://" + URL.hostname,
 project = gl.projects.get(os.getenv("CI_PROJECT_ID"))
 
 
-while 'running' in [lin.attributes['status'] for lin in project.pipelines.list()]:
+def is_job_running():
+    return 'running' in [lin.attributes['status'] for
+                         lin in project.pipelines.list()]
+
+
+while is_job_running() or cinder.volumes.list():
+    print("Woha, another job is running, or there are some volumes left behined ...")
+    print("In any case I'm waiting ... ")
     time.sleep(60)
