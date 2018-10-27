a0b5d30a83f631515d51097504b62012d699b7a5
Oz Tiram <oz.tiram@noris.de>
2018-10-27 11:16:34 +0200

    Refactor: fix all tests

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,19 +1,23 @@
 """
 Test kolt.cloud.builder
 """
+import uuid
 from unittest import mock
 
 import kolt.cloud.openstack
 
 from kolt.cloud.builder import NodeBuilder
+from kolt.ssl import create_certs
 
 
 class DummyServer:  # pylint: disable=too-few-public-methods
     """
     Mock an OpenStack server
     """
-    def __init__(self, name):
+    def __init__(self, name, ip_address):
+
         self.name = name
+        self.ip_address = ip_address
 
 
 NOVA = mock.Mock()
@@ -33,7 +37,8 @@ CONFIG = {
     'storage_class': "Tes tStorageClass"
 }
 
-NOVA.servers.find = mock.MagicMock(return_value=DummyServer("node-1-test"))
+NOVA.servers.find = mock.MagicMock(return_value=DummyServer("node-1-test",
+                                                            "10.32.192.101"))
 NOVA.keypairs.get = mock.MagicMock(return_value='otiram')
 NOVA.glance.find_image = mock.MagicMock(return_value='Ubuntu')
 NOVA.flavors.find = mock.MagicMock(return_value='ECS.C1.4-8')
@@ -61,3 +66,16 @@ def test_node_builder():
 
     assert isinstance(nodes[0], kolt.cloud.openstack.Instance)
     assert nodes[0].name == 'node-1-test'
+
+    certs = create_certs(CONFIG, ['node-1-test'], ['192.168.1.103'],
+                         write=False)
+
+    calico_token = uuid.uuid4().hex[:32]
+    kubelet_token = uuid.uuid4().hex[:32]
+    lb_ip = '212.58.134.78'
+    test_cluster = [DummyServer("master-%d-test",
+                                "10.32.192.10%d" % i) for i in range(1, 4)]
+
+    node_tasks = nb.create_nodes_tasks(certs, kubelet_token,
+                                       calico_token, test_cluster,
+                                       lb_ip)
