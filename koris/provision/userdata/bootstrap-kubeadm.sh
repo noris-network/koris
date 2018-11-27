311a75494bf3f129e135e6faa5b12f20223ddada
Oz Tiram <oz.tiram@noris.de>
2018-11-27 14:34:05 +0100

    Add installation of calico
    [skip ci]

--- koris/provision/userdata/bootstrap-kubeadm.sh
+++ koris/provision/userdata/bootstrap-kubeadm.sh
@@ -230,3 +230,12 @@ wait_for_etcd
 bootstrap_master_2
 bootstrap_master_3
 
+# add calico! we should have these manifests in the base image
+# this will prevent failure if there is a network problem
+curl -O https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+curl -O https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
+sed "s@192.168.0.0/16@"${POD_SUBNET}"@g" calico.yaml
+
+kubectl apply -f rbac-kdd.yaml
+kubectl apply -f calico.yaml
