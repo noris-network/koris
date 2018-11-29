b34b88e637d91e0c661c09530055afcb2f5dd6ab
Oz Tiram <oz.tiram@noris.de>
2018-11-29 15:48:06 +0100

    Update calico.yml after downloading
    Without this flag -i sed simply dumps the modified file to stdout.

--- koris/provision/userdata/bootstrap-k8s-master-ubuntu-16.04.sh
+++ koris/provision/userdata/bootstrap-k8s-master-ubuntu-16.04.sh
@@ -344,7 +344,7 @@ bootstrap_master_3
 curl -LsO https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 curl -LsO https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
-sed "s@192.168.0.0/16@"${POD_SUBNET}"@g" calico.yaml
+sed -i "s@192.168.0.0/16@"${POD_SUBNET}"@g" calico.yaml
 
 kubectl apply -f rbac-kdd.yaml
 kubectl apply -f calico.yaml
