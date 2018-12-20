196ac2b81bc46034d5dbd72bcdf6c62e5e3051bc
Oz Tiram <oz.tiram@noris.de>
2018-12-20 10:23:58 +0100

    Documentation: point to latest koris version
    This little javascript snippet does magic.
    John Resig is my hero.

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,1 +1,11 @@
-console.log("hello")
+const regex = /v\d\.\d{1,2}\.\d{1,2}/gm;
+let title = $(document).find("title").text();
+let version = title.match(regex)[0].substring(1,);
+
+let div = "<div></div><div id=\"korisversion\"><h3>latest koris: <a href=\"https://gitlab.noris.net/PI/koris/tags/v" + version + "\">" + version + "</a></h3><div>"
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $(".sphinxsidebarwrapper").append(div);
+});
