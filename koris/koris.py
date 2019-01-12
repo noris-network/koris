d2ca68c2375cf7694d92fc2157265bff1f7b475e
Oz Tiram <oz.tiram@noris.de>
2019-01-12 22:08:12 +0100

    Update docs of the `add` command.
    By using `---` we can show the users more information about this
    command. Without this the help text will fail.

--- koris/koris.py
+++ koris/koris.py
@@ -90,12 +90,8 @@ class Koris:  # pylint: disable=no-self-use
         role - one of node or master
         amount - the number of worker nodes to add (masters are not supported)
         zone - the availablity zone
-
-        Per default add will use the default kube configuration file. Within this
-        file the default context will  be used. Hence, check which context you
-        have. Before running this command you should issue:
-        kubectl config get-contexts
-
+        ---
+        Add a node to the current active context in your KUBECONFIG.
         You can specify any other configuration file by overriding the
         KUBECONFIG environment variable.
         """
