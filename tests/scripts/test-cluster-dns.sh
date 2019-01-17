299fece58067a55d9e20f83c1d2fe01efc89a786
Julius Lehmann-Richter <julius.lehmann.richter@noris.de>
2019-01-17 08:18:20 +0100

    Make kubeconfig usage explicit again, integrate check_pod code, cosmetics

--- tests/scripts/test-cluster-dns.sh
+++ tests/scripts/test-cluster-dns.sh
@@ -2,23 +2,40 @@
 
 export KUBECONFIG=$1
 
-dns_check_hostname="kubernetes";
-dns_check_hostname="kubernetes";
-dns_check_namespace="default";
-dns_check_cluster_domain="svc.cluster.local";
-dns_check_with_search_domains="${dns_check_hostname}.${dns_check_namespace}";
-dns_check_fqdn="${dns_check_with_search_domains}.${dns_check_cluster_domain}";
+read -r -d '' dns_checkpod <<EOM
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    k8s-app: dnscheck
+  name: dnscheck
+  namespace: default
+spec:
+  containers:
+    - name: dnscheck
+      image: tutum/dnsutils
+      command:
+      - "sleep"
+      args:
+      - "86400"
+EOM
+
+dns_check_hostname="kubernetes"
+dns_check_namespace="default"
+dns_check_cluster_domain="svc.cluster.local"
+dns_check_with_search_domains="${dns_check_hostname}.${dns_check_namespace}"
+dns_check_fqdn="${dns_check_with_search_domains}.${dns_check_cluster_domain}"
 dig="dig +noall +answer -q"
 label="k8s-app=dnscheck"
 
-kubectl apply -f tests/integration/dns-checkpod.yml;
+echo "$dns_checkpod" | kubectl --kubeconfig=${KUBECONFIG} apply -f -
 
-echo -n "Waiting for dnscheck pod to start";
+echo -n "Waiting for dnscheck pod to start"
 
-while [ $(kubectl get pod -l "${label}" -o jsonpath='{.items[0].status.phase}') != "Running" ]; do
-	echo -n ".";
-	sleep 1;
-done;
+while [ $(kubectl --kubeconfig=${KUBECONFIG} get pod -l "${label}" -o jsonpath='{.items[0].status.phase}') != "Running" ]; do
+	echo -n "."
+	sleep 1
+done
 
 function check_dns() {
 	hostname=$1
@@ -27,14 +44,14 @@ function check_dns() {
 
 	echo -e $long_desc
 
-	answer=$(kubectl exec dnscheck -- $dig ${hostname} -t A 2>&1);
+	answer=$(kubectl --kubeconfig=${KUBECONFIG} exec dnscheck -- $dig ${hostname} -t A 2>&1)
 	if [[ $? -ne 0 ]]; then
-		echo -e "\nFailed to resolve ${desc} ${hostname} with check "${dig} $hostname".\nAnswer: ${answer}";
-		kubectl delete pod -l k8s-app=dnscheck;
-		exit 1;
+		echo -e "\nFailed to resolve ${desc} ${hostname} with check "${dig} $hostname".\nAnswer: ${answer}"
+		kubectl --kubeconfig=${KUBECONFIG} delete pod -l k8s-app=dnscheck
+		exit 1
 	else
-		echo -e "Successfully resolved $desc:\n${answer}";
-fi;
+		echo -e "Successfully resolved $desc:\n${answer}"
+fi
 }
 
 check_dns $dns_check_fqdn "FQDN" '\nFirst checking FQDN resolving'
