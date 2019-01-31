cfa0944cef9d350932c0180f113ec1e20bf37063
Oz Tiram <oz.tiram@noris.de>
2019-01-31 15:26:40 +0100

    Add version notifier to koris
    When a new version is published koris will print a message
    that encourages the user to upgrade the version.

---
+++ tests/test_util.py
@@ -0,0 +1,44 @@
+import io
+import unittest.mock
+
+from koris.util.util import KorisVersionCheck
+from koris.util.hue import red
+
+phtml = """
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome to Koris’s ! &#8212; koris v0.9.2-14-g8a53b27 documentation</title>
+</head>
+<body>
+</body>
+</html>
+"""
+
+
+def test_version_is_older():
+    assert KorisVersionCheck(phtml).version == '0.9.2'
+
+    with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as p:
+        KorisVersionCheck(phtml).check_is_latest("0.9.1")
+        val = p.getvalue()
+        xpctd = red("Version 0.9.2 of Koris was released, you should upgrade!") + "\n"
+        assert val == xpctd
+
+
+def test_version_is_the_same():
+    with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as p1:
+        KorisVersionCheck(phtml).check_is_latest("0.9.2")
+        val = p1.getvalue()
+        assert val is ""
+
+
+def test_version_is_newer():
+    with unittest.mock.patch('sys.stdout', new_callable=io.StringIO) as p1:
+        KorisVersionCheck(phtml).check_is_latest("0.9.3.dev57")
+        val = p1.getvalue()
+        assert val is ""
+
+
+def test_web_site_is_not_avail():
+    assert KorisVersionCheck("").version == "0.0.0"
