d083e755992e73ee20b8a7f23c2efcad1341afa8
Alexander Knipping <alexander.knipping@noris.de>
2019-02-08 09:08:52 +0100

    SSL gencert script for testing, renewed certs, WIP dex manifest
    Copied over the gencert.sh script from the dex repo, also renewed SSL
    certs for testing
    
    Various testing around of configuration parameters for getting dex to be
    reachable. Still WIP, sadly not working yet
    
    [skip-ci]

---
+++ dex/gencert.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mkdir -p ssl
+
+cat << EOF > ssl/req.cnf
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+
+[req_distinguished_name]
+
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = dex.example.com
+EOF
+
+# Generate CA Private Key 
+openssl genrsa -out ssl/ca-key.pem 2048
+
+# Generate CA signed with CA Private Key
+openssl req -x509 -new -nodes -key ssl/ca-key.pem -days 365 -out ssl/ca.pem -subj "/CN=kube-ca"
+
+# Generate Serving Certs Private Key
+openssl genrsa -out ssl/key.pem 2048
+
+# Generate Serving Cert Signing Request with Private Key
+openssl req -new -key ssl/key.pem -out ssl/csr.pem -subj "/CN=kube-ca" -config ssl/req.cnf
+
+# Generate Serving Cert with Signing Request and CA 
+openssl x509 -req -in ssl/csr.pem -CA ssl/ca.pem -CAkey ssl/ca-key.pem -CAcreateserial -out ssl/cert.pem -days 10 -extensions v3_req -extfile ssl/req.cnf
