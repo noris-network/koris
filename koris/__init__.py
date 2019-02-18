4f643cf17af09da8b32b1a83c9d6640567a849c8
Oz Tiram <oz.tiram@noris.de>
2019-02-18 20:06:00 +0100

    Bump version to 0.9.2

--- koris/__init__.py
+++ koris/__init__.py
@@ -3,4 +3,4 @@ try:
     import pkg_resources
     __version__ = pkg_resources.get_distribution('koris').version
 except pkg_resources.DistributionNotFound:
-    __version__ = '0.9.1'
+    __version__ = '0.9.2'
