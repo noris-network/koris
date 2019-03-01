1924cab9bd868af936110eacdb871937ec373329
Oz Tiram <oz.tiram@noris.de>
2019-03-01 23:03:13 +0100

    Continue work on integration test for bare-metal
    [skip ci]

--- tests/makefile-bare-metal.mk
+++ tests/makefile-bare-metal.mk
@@ -1,13 +1,52 @@
-.PHONY: create-volumes integration-test-bare-metal
+.PHONY: all create-volumes-masters create-volumes-nodes
 
 IMAGE ?= noris-CentOS7\ [NorisRapidDeploy]
+KEY ?= kube
+NETWORK ?= korispipeline-office-net
+FLAVOR ?= ECS.GP1.2-8
+AZ ?= de-nbg6-1a
+SECGROUP ?= default
 
-create-volumes:
+create-volumes-masters:
+	@echo "creating volumes for masters"
+	@for host in {1..3}; do \
+	openstack volume create --size 25 --bootable --availability-zone $(AZ) --type PI-Storage-Class --image $(IMAGE) root-master-$${host}; done
+
+create-volumes-nodes:
+	@echo "creating volumes for nodes"
+	@openstack volume create --size 25 --bootable --availability-zone $(AZ) --type PI-Storage-Class --image $(IMAGE) root-node-1
+
+
+create-masters: create-volumes-masters
 	for host in {1..3}; do \
-	openstack volume create --size 25 --bootable --availability-zone de-nbg6-1a --type PI-Storage-Class --image $(IMAGE) root-$${host}; done
+	openstack server create --network $(NETWORK) --flavor $(FLAVOR) --availability-zone $(AZ) --key-name $(KEY) \
+		--security-group $(SECGROUP) --volume root-master-$${host} master-$${host}; \
+	done
+
+create-nodes: create-volumes-nodes
+	openstack server create --network $(NETWORK) --flavor $(FLAVOR) --availability-zone $(AZ) --key-name $(KEY) \
+		--security-group $(SECGROUP) --volume root-node-1 node-1;
 
 integration-test-bare-metal: SUBNET ?=sub-korispipeline-office-net
-integration-test-bare-metal: ## simulate installation on bare metal
+integration-test-bare-metal: create-volumes-masters create-volumes-nodes creates-masters create-nodes ## simulate installation on bare metal
 	openstack loadbalancer create --vip-subnet-id $(SUBNET) --name bare-metal
-	openstack server create --min 3 --max 3 --wait --flavor ECS.GP1.2-8 --image noris-CentOS7\ [NorisRapidDeploy] test-bare-metal
-	openstack loadbalancer delete --cascade --name bare-metal
+
+
+create-loadbalancer: SUBNET ?=sub-korispipeline-office-net
+create-loadbalancer:
+	openstack loadbalancer create --vip-subnet-id $(SUBNET) --name bare-metal
+
+clean-lb:
+	openstack loadbalancer delete --cascade bare-metal
+
+clean-instances:
+	for host in {1..3}; do openstack server delete master-$${host}; done
+	openstack server delete node-1
+
+clean-volumes:
+	for host in {1..3}; do openstack volume delete root-master-$${host}; done
+	openstack volume delete root-node-1
+
+clean: clean-instances clean-lb
+
+integration-test: create-volumes-masters create-volumes-nodes create-masters create-nodes create-loadbalancer clean ## simulate installation on bare metal
