4614d376c1edc7c606def695d5327991f9fc6e08
Alexander Knipping <alexander.knipping@noris.de>
2019-04-02 17:19:26 +0200

    Added koris.constants
    The koris.constants file defines the default names
    for the master listener and pool.
    
    /skip-end-to-end-tests/

---
+++ koris/constants.py
@@ -0,0 +1,5 @@
+"""Defines constants that are used throught the koris project"""
+
+MASTER_PREFIX = "master"
+MASTER_LISTENER_NAME = f"{MASTER_PREFIX}-listener"
+MASTER_POOL_NAME = f"{MASTER_PREFIX}-pool"
