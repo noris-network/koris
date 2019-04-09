3a3734b64de63a91c18338eb28cc840dc034286a
Oz Tiram <oz.tiram@noris.de>
2019-04-09 22:17:48 +0200

    Retry DNS check a few times before failing
    It could very well be that calico is still not up and running
    or that CoreDNS is not running.

--- tests/scripts/test-cluster-dns.sh
+++ tests/scripts/test-cluster-dns.sh
@@ -1,6 +1,28 @@
 
 export KUBECONFIG=$1
 
+function fail {
+  echo "$1" >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=5
+  local delay=15
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
 read -r -d '' dns_checkpod <<EOM
 apiVersion: v1
 kind: Pod
@@ -26,7 +48,7 @@ echo "$dns_checkpod" | kubectl apply -f -
 
 echo -n "Waiting for dnscheck pod to start"
 
-while [ $(kubectl get pod -l "${label}" -o jsonpath='{.items[0].status.phase}') != "Running" ]; do
+while [ "$(kubectl get pod -l "${label}" -o jsonpath='{.items[0].status.phase}')" != "Running" ]; do
 	echo -n "."
 	sleep 1
 done
@@ -36,19 +58,18 @@ function check_dns() {
     opts=$2
 	desc=$3
 
-	echo -e $long_desc
-
-	answer=$(kubectl exec dnscheck -- $dig ${hostname} $opts -t A 2>&1)
-	if [[ $? -ne 0 || -z ${answer} ]]; then
-		echo -e "\nFailed to resolve ${desc} ${hostname} with check "${dig} $hostname".\nAnswer: ${answer}"
+	answer="$(kubectl exec dnscheck -- ${dig} "${hostname}" ${opts} -t A 2>&1)"
+	if [[ $? -ne 0 || -z "${answer}" ]]; then
+		echo -e "\nFailed to resolve ${desc} ${hostname} with check ${dig} $hostname.\nAnswer:" "${answer}"
 		#kubectl delete pod -l k8s-app=dnscheck
 		exit 1
 	else
 		echo -e "Successfully resolved $desc:\n${answer}"
     fi
 }
-check_dns "kubernetes.default.svc.cluster.local" "" "FQDN"
-check_dns "kubernetes.default" "+search" "namespace"
-check_dns "kubernetes" "+search"  "short name"
+
+retry check_dns "kubernetes.default.svc.cluster.local" "" "FQDN"
+retry check_dns "kubernetes.default" "+search" "namespace"
+retry check_dns "kubernetes" "+search"  "short name"
 
 # vim: tabstop=4 shiftwidth=4
