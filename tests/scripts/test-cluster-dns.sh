1750a8478c9e6b93b732b7aa80ab749e83b8d3b1
Oz Tiram <oz.tiram@noris.de>
2019-04-10 08:26:08 +0200

    Pipeline: try and make test-cluster-dns robuster

--- tests/scripts/test-cluster-dns.sh
+++ tests/scripts/test-cluster-dns.sh
@@ -15,10 +15,10 @@ function retry {
       if [[ $n -lt $max ]]; then
         ((n++))
         echo "Command failed. Attempt $n/$max:"
-        sleep $delay;
       else
         fail "The command has failed after $n attempts."
       fi
+	sleep $delay;
     }
   done
 }
@@ -44,14 +44,20 @@ EOM
 dig="dig +noall +answer -q"
 label="k8s-app=dnscheck"
 
+echo -n "Waiting for CoreDNS pod to start"
+while [ "$(kubectl get pod -l k8s-app=kube-dns -n kube-system -o jsonpath='{.items[0].status.phase}' 2>/dev/null)" != "Running" ]; do
+	echo -n "."
+    sleep 1
+done
+
 echo "$dns_checkpod" | kubectl apply -f -
 
 echo -n "Waiting for dnscheck pod to start"
-
 while [ "$(kubectl get pod -l "${label}" -o jsonpath='{.items[0].status.phase}')" != "Running" ]; do
 	echo -n "."
-	sleep 1
+    sleep 1
 done
+echo ""
 
 function check_dns() {
 	hostname=$1
@@ -68,8 +74,11 @@ function check_dns() {
     fi
 }
 
-retry check_dns "kubernetes.default.svc.cluster.local" "" "FQDN"
-retry check_dns "kubernetes.default" "+search" "namespace"
-retry check_dns "kubernetes" "+search"  "short name"
+for i in {1..5}; do
+    check_dns "kubernetes.default.svc.cluster.local" "" "FQDN" && break || sleep 15;
+done
 
+check_dns "kubernetes.default" "+search" "namespace"
+check_dns "kubernetes" "+search"  "short name"
+#
 # vim: tabstop=4 shiftwidth=4
