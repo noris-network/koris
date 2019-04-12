6e7d549202bdfecfd589c33ec46ac60fa2a879f5
Oz Tiram <oz.tiram@noris.de>
2019-04-12 13:18:31 +0200

    Add python-gitlab to dev dependencies

--- tests/scripts/protect-un-protect.py
+++ tests/scripts/protect-un-protect.py
@@ -1,4 +1,9 @@
 #!/bin/python3
+"""
+A script to protect or unprotect a branch - used in our release process
+
+Usage: script.py <branch-name>
+"""
 import os
 import sys
 
@@ -7,15 +12,20 @@ from urllib.parse import urlparse
 import gitlab
 
 
-URL = urlparse(os.getenv("CI_PROJECT_URL", "https://gitlab.noris.net/PI/koris/"))
-gl = gitlab.Gitlab(URL.scheme + "://" + URL.hostname,
-                   private_token=os.getenv("ACCESS_TOKEN"))
+def main():
+    URL = urlparse(os.getenv("CI_PROJECT_URL", "https://gitlab.noris.net/PI/koris/"))
+    gl = gitlab.Gitlab(URL.scheme + "://" + URL.hostname,
+                       private_token=os.getenv("ACCESS_TOKEN"))
 
-project = gl.projects.get(os.getenv("CI_PROJECT_ID", 1260))
+    project = gl.projects.get(os.getenv("CI_PROJECT_ID", 1260))
 
-branch = project.branches.get(sys.argv[1])
+    branch = project.branches.get(sys.argv[1])
 
-if branch.attributes['protected']:
-    branch.unprotect()
-else:
-    branch.protect()
+    if branch.attributes['protected']:
+        branch.unprotect()
+    else:
+        branch.protect()
+
+
+if __name__ == "__main__":
+    main()
