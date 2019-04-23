7d64480444c32ec99eba5ca737ffd1742bc19dca
Jonas Röckl <jonas.roeckl@noris.de>
2019-04-23 14:10:11 +0200

    added code for download artifacts

--- tests/scripts/coverage_report.py
+++ tests/scripts/coverage_report.py
@@ -6,6 +6,7 @@ A script to generate a comprehensive coverage report based on a set of
 
 import gitlab
 import os
+import subprocess
 import sys
 
 from urllib.parse import urlparse
@@ -13,6 +14,17 @@ from urllib.parse import urlparse
 
 CI_PROJECT_URL = "https://gitlab.noris.net/PI/koris/"
 CI_PROJECT_ID = "1260"
+
+# name of the job and artifact files to use from this job
+FILES = {
+    "unittest": [".coverage.unit-test"],
+    "build-cluster": [".coverage.build-cluster"],
+    "add-master": [".coverage.add-master"],
+    "add-nodes": [".coverage.add-nodes"],
+    "delete-added-master": [".coverage.delete-master"],
+    "delete-added-node": [".coverage.delete-nodes"],
+    "cleanup": [".coverage.destroy"],
+}
 
 
 def main(argv):
@@ -25,7 +37,19 @@ def main(argv):
     jobs = pipeline.jobs.list(all=True)
 
     for job in jobs:
-        print(job)
+        # check if we have a job we want to extract data from
+        if job.name in FILES:
+            print(job.name)  # TODO: remove if we know it works
+
+            # find artifact archive that cotnaints the interesting files
+            zipfn = "___artifacts.zip"
+            with open(zipfn, "wb") as f:
+                job.artifacts(streamed=True, action=f.write)
+                subprocess.run(["unzip", "-bo", zipfn])
+
+                subprocess.run(["ls", "-la"])  # TODO: remove if it works
+
+                os.unlink(zipfn)
 
 
 if __name__ == "__main__":
