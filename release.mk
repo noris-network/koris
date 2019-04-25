cea3cad317514bb81f6bb76c985617a855d2ad78
Oz Tiram <oz.tiram@noris.de>
2019-04-25 14:28:17 +0200

    Abort running pipeline twice for a release
    When doing a release we push the master branch and a tag which
    refer the latest commit on the master branch. Gitlab is stupidly
    running a CI job for both. We cancel running the job for the master
    branch because it's redundant.

--- release.mk
+++ release.mk
@@ -1,3 +1,5 @@
+PY ?= python3
+
 check-env:
 ifndef VER
 	$(error VER is undefined)
@@ -7,6 +9,30 @@ check-api-key:
 ifndef ACCESS_TOKEN
 	$(error ACCESS_TOKEN is undefined)
 endif
+
+
+define ABORT_PIPELINE
+import os
+import sys
+
+from urllib.parse import urlparse
+
+import gitlab
+
+URL = urlparse(os.getenv("CI_PROJECT_URL", "https://gitlab.noris.net/PI/koris/"))
+PROJECT_ID=os.getenv("CI_PROJECT_ID", 1260)
+print(PROJECT_ID)
+gl = gitlab.Gitlab(URL.scheme + "://" + URL.hostname, private_token=os.getenv("ACCESS_TOKEN"))
+
+project = gl.projects.get(PROJECT_ID)
+
+for pl in project.pipelines.list():
+    if pl.attributes['ref'] == 'master':
+        pl.cancel()
+        print("Caneclled pipeline for master")
+
+endef
+export ABORT_PIPELINE
 
 start-release: check-env
 	@echo "checking out branch prepare_"$(VER)
@@ -22,7 +48,7 @@ do-bump: check-env
 	echo $(NVER)
 	sed -i "s/[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+/$(NVER)/g" koris/__init__.py
 
-abort-release:
+abort-release: check-ver
 	git checkout dev
 	git branch -D prepare_$(VER)
 	git tag -d $(VER)
@@ -36,6 +62,10 @@ do-release: do-bump check-api-key
 	python3 tests/scripts/protect-un-protect.py master
 	git push origin master --tags
 	python3 tests/scripts/protect-un-protect.py master
+	@$(PY) -c "$$ABORT_PIPELINE"
+
+abort-pipeline: check-api-key
+	@$(PY) -c "$$ABORT_PIPELINE"
 
 finish-release: check-env check-api-key
 	git checkout dev
@@ -46,5 +76,8 @@ finish-release: check-env check-api-key
 	python3 tests/scripts/protect-un-protect.py dev
 	git push origin dev
 	python3 tests/scripts/protect-un-protect.py dev
+
+
+
 
 # vim: tabstop=4 shiftwidth=4
