1106e11d04b9cf3338954f0023141428a3cad6a3
Oz Tiram <oz.tiram@noris.de>
2019-07-18 07:33:12 +0200

    Bump version to v1.1.3

--- koris/__init__.py
+++ koris/__init__.py
@@ -3,7 +3,7 @@ try:
     import pkg_resources
     __version__ = pkg_resources.get_distribution('koris').version
 except pkg_resources.DistributionNotFound:
-    __version__ = '1.1.2'
+    __version__ = '1.1.3'
 
 # Defining some constants
 MASTER_PREFIX = "master"
