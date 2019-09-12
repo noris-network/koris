5d86e8619b729e70266229557850a17271083caa
Alexander Knipping <alexander.knipping@noris.de>
2019-09-12 16:16:57 +0200

    Change koris latest link in custom.js

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -2,7 +2,7 @@ const regex = /v\d\.\d{1,2}\.\d{1,2}\w*/gm;
 let title = $(document).find("title").text();
 let version = title.match(regex)[0].substring(1,);
 
-let div = "<div></div><div id=\"korisversion\"><h3>latest koris: <a href=\"https://gitlab.noris.net/PI/koris/tags/v" + version + "\">" + version + "</a></h3><div>"
+let div = "<div></div><div id=\"korisversion\"><h3>latest koris: <a href=\"https://gitlab.com/noris-network/koris/tags/v" + version + "\">" + version + "</a></h3><div>"
 
 
 $( document ).ready(function() {
